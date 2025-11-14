55a3284102f1f1f7d265ce0cf8ace68165ce449b
Luciano <ovejeromarcosluciano@gmail.com>
2025-11-14 00:21:52 -0300

    creando viewmodels

--- ViewModels/ProductosViewModels.cs
+++ ViewModels/ProductosViewModels.cs
@@ -1,7 +1,13 @@
-
+namespace SistemaVentas.Web.ViewModels;
+using System.ComponentModel.DataAnnotations;
 public class ProductosViewModels
 {
     public int idProducto { get; set; }
+    [Display(Name = "Descripción del Producto")]
+    [StringLength(250, ErrorMessage = "La descripción no puede superar los 250 caracteres.")]
     public string descripcion { get; set; }
-    public int precio { get; set; }
+    [Display(Name = "Precio Unitario")]
+    [Required(ErrorMessage = "El precio es obligatorio.")]
+    [Range(0.01, double.MaxValue, ErrorMessage = "El precio debe ser un valor positivo.")]
+    public decimal precio { get; set; }
 }
--- obj/Debug/net8.0/tl2-tp8-2025-Sheepherr.AssemblyInfo.cs
+++ obj/Debug/net8.0/tl2-tp8-2025-Sheepherr.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("tl2-tp8-2025-Sheepherr")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bfb41d208aabcb0c1346114bc0b410dd643793c7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c1a6fa1eadefa120ffb3ff9964f8eed29d5089bd")]
 [assembly: System.Reflection.AssemblyProductAttribute("tl2-tp8-2025-Sheepherr")]
 [assembly: System.Reflection.AssemblyTitleAttribute("tl2-tp8-2025-Sheepherr")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
